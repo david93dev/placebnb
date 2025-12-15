80907030c1e263a16cef1bf4c01063edb6c54b06
david93dev <david.flor@maisunifacisa.com.br>
2025-12-15 09:53:13 -0300

    feat: create post login

--- back/domains/users/routes.js
+++ back/domains/users/routes.js
@@ -37,4 +37,25 @@ router.post("/", async (req, res) => {
     
 })
 
+router.post("/login", async (req, res) => {
+    connectDb();
+
+    const { email, password } = req.body;
+
+    try {
+        const userDoc = await User.findOne({email})
+
+        if (userDoc) {
+            const { _id, name} = userDoc;
+            const passwordCorrect = bcrypt.compareSync(password, userDoc.password)
+            passwordCorrect ? res.json({ _id, name, email}) : res.status(400).json("Senha inválida")
+        } else {
+            res.status(400).json('Senha inválida!')
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 export default router;
