87c06d71a080e8100af1921a09f67f3f88ca07c2
david93dev <david.flor@maisunifacisa.com.br>
2025-12-15 17:16:19 -0300

    chore: install cookieParser

--- back/app.js
+++ back/app.js
@@ -2,13 +2,18 @@ import express from "express";
 import "dotenv/config";
 import UserRoutes from "./domains/users/routes.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const { PORT } = process.env;
 
-app.use(express.json())
-app.use(cors());
-app.use("/users", UserRoutes)
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials: true,
+}));
+app.use("/users", UserRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor está rodando...✅\nPort: ${PORT}`)
