d2bf02e26cb1c40ca82f4e0e69ca66da40e98a9c
david93dev <david.flor@maisunifacisa.com.br>
2025-12-15 09:56:38 -0300

    chore: install axios

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -7,7 +8,7 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submit disparado", email);
+    a
   };
 
   return (
