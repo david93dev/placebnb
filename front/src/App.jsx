b1c2bee992b750b6a64e7c6f6fe926e86b9ebf57
david93dev <david.flor@maisunifacisa.com.br>
2025-12-16 11:34:12 -0300

    feat: config context

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,39 +3,41 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
 import Register from "./pages/Register";
-import { useEffect } from "react";
 import Account from "./pages/Account";
+import { UserContextProvider } from "./contexts/UserContext";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const axiosGet = async () => {
-      const { data } = await axios.get('/users/profile');
-      
-      setUser(data);
-    }
-    axiosGet();
-  }, []);
 
   return (
-    <BrowserRouter>
-    
-      <Header user={user} />
+    <UserContextProvider>
+      <BrowserRouter>
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login user={user} setUser={setUser} />} />
-        <Route path="/register" element={<Register setUser={setUser} />} />
-        <Route path="/account/:subpage?" element={<Account user={user} />} />
-      </Routes>
+        <Routes>
 
-    </BrowserRouter>
+          <Route 
+          path="/"
+           element={<Home />} />
+
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route 
+          path="/register" 
+          element={<Register />} />
+
+          <Route 
+          path="/account/:subpage?" 
+          element={<Account />} />
+
+        </Routes>
+      </BrowserRouter>
+    </UserContextProvider>
   );
 }
 
