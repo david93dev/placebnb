6a565ea4d7530e08e3c2954620a1f809884d7f7d
david93dev <david.flor@maisunifacisa.com.br>
2025-12-16 09:11:45 -0300

    refactor: users tokens

--- back/domains/users/routes.js
+++ back/domains/users/routes.js
@@ -5,92 +5,88 @@ import User from "./model.js";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
-
 const router = Router();
 const bcryptSalt = bcrypt.genSaltSync();
-const { JWT_SECRET_KEY } = process.env
+const { JWT_SECRET_KEY } = process.env;
 
 router.get("/", async (req, res) => {
-    connectDb();
+  connectDb();
 
-    try {
-        const userDoc = await User.find();
-        res.json(userDoc)
-    } catch (error) {
-        res.json(500).json(error)
-    }
+  try {
+    const userDoc = await User.find();
+    res.json(userDoc);
+  } catch (error) {
+    res.json(500).json(error);
+  }
 });
 
 router.post("/", async (req, res) => {
-    connectDb();
+  connectDb();
 
-    const { name, email, password } = req.body;
-    const encryptedPassword = bcrypt.hashSync(password, bcryptSalt)
+  const { name, email, password } = req.body;
+  const encryptedPassword = bcrypt.hashSync(password, bcryptSalt);
 
-    try {
-        const newUserDoc = await User.create({
-            name,
-            email,
-            password: encryptedPassword,
-        })
+  try {
+    const newUserDoc = await User.create({
+      name,
+      email,
+      password: encryptedPassword,
+    });
 
-        const { _id } = newUserDoc;
-        const newUserObj = { name, email, _id }
+    const { _id } = newUserDoc;
+    const newUserObj = { name, email, _id };
 
-        const token = jwt.sign(newUserObj, JWT_SECRET_KEY);
+    jwt.sign(newUserObj, JWT_SECRET_KEY, {}, (error, token) => {
+      if (error) throw error;
 
-        res.cookie("token", token).json(newUserObj)
-    } catch (error) {
-        res.json(500).json(error)
-    }
-    
-})
+      res.cookie("token", token).json(newUserObj);
+    });
+  } catch (error) {
+    res.json(500).json(error);
+    throw error;
+  }
+});
 
 router.post("/login", async (req, res) => {
-    connectDb();
+  connectDb();
 
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const userDoc = await User.findOne({email})
+  try {
+    const userDoc = await User.findOne({ email });
 
-        if (userDoc) {
+    if (userDoc) {
+      const { _id, name } = userDoc;
+      const passwordCorrect = bcrypt.compareSync(password, userDoc.password);
 
-            const { _id, name} = userDoc;
-            const passwordCorrect = bcrypt.compareSync(password, userDoc.password)
+      if (passwordCorrect) {
+        const newUserObj = { _id, name, email };
+        const token = jwt.sign(newUserObj, JWT_SECRET_KEY);
 
-            if (passwordCorrect) {
-                const newUserObj = { _id, name, email}
-                const token = jwt.sign(newUserObj, JWT_SECRET_KEY)
-
-                res.cookie("token", token).json(newUserObj)
-            } else {
-                res.status(400).json("Senha inválida")
-            }
-           
-        } else {
-            res.status(400).json('Senha inválida!')
-        }
-
-    } catch (error) {
-        res.status(500).json(error)
+        res.cookie("token", token).json(newUserObj);
+      } else {
+        res.status(400).json("Senha inválida");
+      }
+    } else {
+      res.status(400).json("Senha inválida!");
     }
-})
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 router.get("/profile", async (req, res) => {
-    const { token } = req.cookies;
+  const { token } = req.cookies;
 
-    if (token) {
-        try {
-            const userInfo = jwt.verify(token, JWT_SECRET_KEY)
-            res.json(userInfo);
-        } catch (error) {
-            res.json(500).json(error);
-        }
-    } else {
-        res.json(null);
-    }
+  if (token) {
+    jwt.verify( token, JWT_SECRET_KEY, {}, (error, userInfo) => {
+        if (error) throw error;
 
+        res.json(userInfo);
+      });
+ } else {
+    res.json(null);
+  }
 });
 
 export default router;
