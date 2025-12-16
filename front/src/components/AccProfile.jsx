66b0222929522e82c4e75c80c339cc73fdf156cd
david93dev <david.flor@maisunifacisa.com.br>
2025-12-16 11:04:02 -0300

    feate: create new component profile

---
+++ front/src/components/AccProfile.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+const AccProfile = ({ user }) => {
+  const [redirect, setRedirect] = useState(false);
+
+  const logout = async () => {
+    try {
+      const { data } = await axios.post("/users/logout");
+
+      setRedirect(true);
+    } catch (error) {
+        alert(JSON.stringify(error))
+    }
+  };
+
+  if (redirect) return <Navigate to={"/"} />
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <p>
+        Logado com {user?.name} ({user?.email})
+      </p>
+
+      <button
+        onClick={logout}
+        className="bg-primary-400 hover:bg-primary-500 min-w-44 cursor-pointer rounded-full px-4 py-2 font-semibold text-white transition-colors duration-300"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default AccProfile;
